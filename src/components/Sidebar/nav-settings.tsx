24d7215e4cb311003fb26fcea62dc61a07cd927e
Jack Sparrow <108980635+Jackspparowhs@users.noreply.github.com>
2025-12-14 22:01:37 +0530

    Update nav-settings.tsx

--- src/components/Sidebar/nav-settings.tsx
+++ src/components/Sidebar/nav-settings.tsx
@@ -16,15 +16,16 @@ export function NavSettings() {
   return (
     <SidebarGroup>
       <SidebarGroupLabel className="gap-2">
-        <span>Tuỳ chỉnh</span>
+        {/* CHANGED: Translated 'Tuỳ chỉnh' */}
+        <span>Settings</span>
         <Tooltip>
           <TooltipTrigger asChild>
             <CloudOff size={18} className="cursor-pointer" />
           </TooltipTrigger>
           <TooltipContent>
+            {/* CHANGED: Translated tooltip warning */}
             <p>
-              Những tuỳ chỉnh này chỉ có hiệu lực trên thiết bị hiện tại, không
-              đồng bộ theo tài khoản!
+              These settings only apply to this device and do not sync with your account!
             </p>
           </TooltipContent>
         </Tooltip>
